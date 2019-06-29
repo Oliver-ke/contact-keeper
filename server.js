1b42f2ae677170d90571c3df7c112857e1da3881
azorji kelechi oliver <kelechioliver96@gmail.com>
2019-06-29 11:21:15 +0100

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 // Routes import
 
@@ -19,6 +20,13 @@ app.use(express.json({ extended: false }));
 app.use('/api/contacts', contacts);
 app.use('/api/users', users);
 app.use('/api/auth', auth);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+	// set static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
