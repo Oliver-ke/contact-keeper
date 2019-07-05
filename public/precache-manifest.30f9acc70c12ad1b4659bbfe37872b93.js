85416cdf6dddcdbcd1fda51dcf1eb75bc24db3f7
azorji kelechi oliver <kelechioliver96@gmail.com>
2019-07-05 12:17:34 +0100

    heroku bug fix

---
+++ public/precache-manifest.30f9acc70c12ad1b4659bbfe37872b93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "092616ef5fdddd12919e88dd77a5ee7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a4b14fd452e562debc2",
+    "url": "/static/css/main.269bb629.chunk.css"
+  },
+  {
+    "revision": "441c4e9d2ec760fbb146",
+    "url": "/static/js/2.4b2cf415.chunk.js"
+  },
+  {
+    "revision": "0a4b14fd452e562debc2",
+    "url": "/static/js/main.53e836e0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cffa9d9d88890cfcd0ae3d6dd7a6efa9",
+    "url": "/static/media/spinner.cffa9d9d.gif"
+  }
+]);
--- server.js
+++ server.js
@@ -24,8 +24,10 @@ app.use('/api/auth', auth);
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
 	// set static folder
-	app.use(express.static('client/build'));
-	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+app.use(express.static('public'));
+app.get('*',(req,res) =>{
+    res.sendFile(path.resolve(_dirname,'public','index.html'));
+});
 }
 
 const PORT = process.env.PORT || 5000;
